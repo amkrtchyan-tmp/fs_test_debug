b9d76a32ca60142f9ec3cae4da276e07a54d387b
Armen Mkrtchyan <amkrtchyan@techdaisy.co>
2023-09-16 10:36:09 +0400

    Cleaned up

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,10 @@
-import os
-
 import pytest
 
 from fapp import app
-
-os.environ['APP_CONF'] = 'testing'
 
 
 @pytest.fixture(scope='session')
 def fapp():
     """Creating app and context"""
-    # for APP to start in testing mode please set APP_CONFIG=testing environment variable
     with app.app_context():
         yield app
